5ce64938e12d401fe6f37d02cd5e7386f0613ac9
goniux <srivastavaakshat882@gmail.com>
2026-02-15 07:23:43 +0530

    fix layout types

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,11 +1,16 @@
-import Providers from "./providers"
+import Providers from "./providers";
+import { ReactNode } from "react";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <html>
       <body>
         <Providers>{children}</Providers>
       </body>
     </html>
-  )
+  );
 }
